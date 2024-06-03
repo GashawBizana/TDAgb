7b49dbc0c2a58e831b8b72538f3db311388d5c96
GashawBizana <117026056+GashawBizana@users.noreply.github.com>
2024-06-03 16:32:54 -0400

    Update AJS_shear.py

--- Scripts_for_automated_simulation_in_HPC/AJS_shear.py
+++ Scripts_for_automated_simulation_in_HPC/AJS_shear.py
@@ -7,15 +7,12 @@ script_path="./job_ShearCoupling_constant_stress.sh"
 with open('GB_names.txt') as gn:
     GB_type = [line.rstrip()[:-4] for line in gn]
 
-GB_type = ['struct_S7_fcc_N4_1_2_Al_M99_210802.1717','struct_S13a_fcc_N0_n5_1_Al_M99_201228.449','struct_S25a_fcc_N0_1_7_Al_M99_220118.4463','struct_S29b_fcc_N2_7_11_Al_M99_210713.4193']
-GB_type=GB_type[1:4]
-print(GB_type)
-#T_values = [500,750,250]
+
 T_values=[250,350,400,500,550,600]
-#tau_values = [7000,1000,3000]
+
 tau_values=[100,1300,1500,1700,2000,2300]
 latP_values = [4.0449,4.0544,4.0594,4.0699,4.0755,4.0812]
-#latP_values=[4.0699,4.0966,4.0449]
+
 
 
 
